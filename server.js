70c3d8f93410d9344ea29daf22fed0e72c246e9b
Assetbekov-Almar <almar.asetbekov@gmail.com>
2022-11-11 15:23:38 +0600

    Feature: Add self signed SSL certificate

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const https = require('https')
+const fs = require('fs')
 const path = require('path')
 const express = require('express')
 
@@ -5,10 +7,17 @@ const PORT = 3000
 
 const app = express()
 
+app.get('/secret', (req, res) => {
+	return res.send('Secret')
+})
+
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
-app.listen(PORT, () => {
+https.createServer({
+	key: fs.readFileSync('key.pem'),
+	cert: fs.readFileSync('cert.pem')
+}, app).listen(PORT, () => {
 	console.log(`Listening on PORT ${PORT}...`)
 })
